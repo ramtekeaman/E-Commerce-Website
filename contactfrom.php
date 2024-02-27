a0686d55ee83c275ec8ce355c31dbd2a99d92f89
ramtekeaman <amanramteke000@gmail.com>
2024-02-27 23:49:59 +0530

    first commit

---
+++ contactfrom.php
@@ -0,0 +1,144 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="icon" type="image/x-icon" href="/images/AMAN-logos_white.png">
+    <link rel="stylesheet" href="style.css" />
+    <link rel="shortcut icon" href="/pages/icon-images/phone-call_455705.png" type="image/x-icon" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <title>Contact Us</title>
+</head>
+
+<body>
+    <div class="navbar">
+
+        <div class="topnav">
+            <div class="img-top">
+                <img src="/images/AMAN-logos_black.png" alt="">
+            </div>
+            <a class="active" href="http://127.0.0.1:5502/home.html">Home</a>
+            <a href="/about_us.html">About us</a>
+            <a href="/Products.html">Products</a>
+            <a href="/pooja.html">Pooja Room</a>
+            <a href="/contact.html">Contact us</a>
+
+        </div>
+    </div>
+    <div class="contact-container">
+        <div class="sign-up-head">
+            <h1>Contact Us</h1>
+            <p>Get in Touch with Us!Questions, feedback, or just want to say hello? We'd love to hear from you.
+            </p>
+            <p>Reach out through the
+                options below and let's start a conversation!</p>
+        </div>
+        <div class="left-right">
+            <div class="left-side">
+                <h1>Get in Touch</h1>
+                <h3>Contact Information</h3>
+                <p>we are here for you and your all diffciulties</p>
+
+                <div class="contact-details">
+                    <div class="item">
+                        <span class="material-symbols-outlined"> call </span>
+                        <a href="tel:9322898678">+91 9322898678</a>
+                    </div>
+                    <div class="item">
+                        <span class="material-symbols-outlined"> mail </span>
+                        <a href="mailto:amanramteke000@gmail.com">amanramteke000@gmail.com</a>
+
+                    </div>
+                    <div class="item">
+                        <span class="material-symbols-outlined"> location_on </span>
+                        <p><a href="https://www.google.com/maps/dir/21.1794516,79.0581608/32-A,+Wardha+Rd,+Chatrapati+Square,+Gajanan+Nagar,+Nagpur,+Maharashtra+440015/@21.1455522,79.0266962,13z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bd4bf7a2edb1415:0x3855ec6cb9a3bf3f!2m2!1d79.0705675!2d21.1110313?entry=ttu">32-A, Wardha Rd, Chatrapati Square, Pragati Colony, Nagpur, Maharashtra 440015</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="right-side">
+                <div class="sign-up-middle">
+                <form action="save.php" method="post">
+           <input type="text" name="fname"   class="form-control" placeholder="Enter First Name"> <br>
+           <input type="text" name="lname"  class="form-control" placeholder="Enter last Name"> <br>
+           <input type="email" name="email"  class="form-control" placeholder="Enter Email"> <br>
+           <input type="text" name="mobile"  class="form-control" placeholder="Enter Mobile Number"> <br>
+           
+                </div>
+                <div class="sub">
+                <form action="save.php" method="post">
+                <input type="submit" class="form-control submit" value="submit" >
+       </form>
+                
+               
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>company</h4>
+                    <ul>
+                        <li><a href="/about_us.html">about us</a></li>
+                        <li><a href="#">our services</a></li>
+                        <li><a href="#">privacy policy</a></li>
+                        <li><a href="#">affiliate program</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>get help</h4>
+                    <ul>
+                        <li><a href="/contact.html">FAQ</a></li>
+                        <li><a href="#">shipping</a></li>
+                        <li><a href="#">returns</a></li>
+                        <li><a href="#">order status</a></li>
+                        <li><a href="#">payment options</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>online shop</h4>
+                    <ul>
+                        <li><a href="/Products.html">watch</a></li>
+                        <li><a href="#">bag</a></li>
+                        <li><a href="#">shoes</a></li>
+                        <li><a href="#">dress</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Address</h4>
+                    <div class="address">
+                        <p>32-A, Wardha Rd, Chatrapati Square, 
+                            Pragati Colony, Nagpur, Maharashtra 440015
+                        </p>
+                    </div>
+                    <h4>follow us</h4>
+                    <div class="social-links">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                     
+                        
+                    </div>
+
+
+                </div>
+                
+            </div>
+        </div>
+        <div class="cp">
+            <h5>Copyright © 2024 AmanDoors. All Rights Reserved.
+            </h5>
+        </div>
+        </div>
+        
+    </footer>
+</body>
+
+</html>
